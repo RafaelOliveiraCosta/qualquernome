8634859bbab3c23b60597d510d0b2f517a48b08a
unknown <bobaorafaelchatao@gmail.com>
2021-05-21 15:33:02 -0300

    qualquercoisa

---
+++ login.js
@@ -0,0 +1,13 @@
+let uzhiwiwhiwhzi = "Rafael";
+let uzhiwiwhiwhzisdf = '123';
+
+function retornarLOGIN(){
+    if(prompt('Login') === uzhiwiwhiwhzi && prompt('Senha') === uzhiwiwhiwhzisdf){
+       document.body.innerHTML = `
+      
+       `;
+    }
+    else{
+        alert('errado')
+    }
+}
